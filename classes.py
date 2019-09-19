b146d7c81bda7e7f2065bd30f500b485fbd51098
harbar20 <30803341+harbar20@users.noreply.github.com>
2019-09-19 15:11:30 -0400

    finished import_showdowen_team command
    finished the import command, now the Team object can be updated and changed based on the imported showdown text.

--- classes.py
+++ classes.py
@@ -1,7 +1,19 @@
 import pokebase as pb
+
+#getting a list of all pokemon names currently
+with open("pokemon_names.txt") as f:
+    pokemon = [i.replace("\n", "") for i in f]
+    
+#function to find nth instance of substring in a string
+def find_nth(haystack, needle, n):
+    start = haystack.find(needle)
+    while start >= 0 and n > 1:
+        start = haystack.find(needle, start+len(needle))
+        n -= 1
+    return start
     
 class Pokemon():
-    def __init__(self, pokemonName, nickname="", level="100", gender="F", isShiny=False,
+    def __init__(self, pokemonName, nickname="", isShiny=False,
                 moves=["", "", "", ""], item="", ability="",
                 EVs={"HP":0,
                      "Atk":0,
@@ -19,8 +31,6 @@ class Pokemon():
                     }, nature=""):
         self.name = pokemonName
         self.nickname = nickname
-        self.level = level
-        self.gender = gender
         self.isShiny = isShiny
         self.moves = moves
         self.item = item
@@ -33,10 +43,6 @@ class Pokemon():
         return self.name
     def get_nickname(self):
         return self.nickname 
-    def get_level(self):
-        return self.level
-    def get_gender(self):
-        return self.gender
     def get_isShiny(self):
         return self.isShiny
     def get_moves(self):
@@ -53,14 +59,7 @@ class Pokemon():
         return self.iv
     
     def change_nickname(self, newNickname):
-        self.nickname = newNickname  
-    def change_level(self, newLevel):
-        self.level = newLevel  
-    def toggle_gender(self):
-        if self.gender == 'F':
-            self.gender == 'M'
-        else:
-            self.gender = 'F'      
+        self.nickname = newNickname      
     def toggle_isShiny(self):
         if self.isShiny:
             self.isShiny = False
@@ -94,7 +93,86 @@ class Team():
         """
         Imports showdown teams and saves it as the team.
         """
-        pass
+        #separating the text by pokemon
+        pokemonTextList = showdown_text.split("\n\n")
+        
+        #going through each pokemon data in the list
+        for pokemonData in pokemonTextList:
+            counter = 2
+            #getting name/nickname from pokemon
+            currentIndex = pokemonData.find("@") - 1
+            name = pokemonData[:currentIndex]
+            #looking through list of all pokemon names to see if they have nicknames
+            if name not in pokemon:
+                nickname = name[:pokemonData.find("(") - 1]
+                name = name[pokemonData.find("(")+1:pokemonData.find(")")]
+            else:
+                nickname = ""
+            #print("Nickname: ", currentIndex)
+            
+            #getting the item of the pokemon
+            currentIndex += 3
+            item = pokemonData[currentIndex:pokemonData.find("\n")]
+            #print("Item: ", currentIndex)
+            
+            #getting the ability of the pokemon
+            currentIndex = pokemonData.find("\n") + 10
+            ability = pokemonData[currentIndex:find_nth(pokemonData, "\n", counter)]
+            #print("Ability: ", currentIndex)
+            
+            #checking if the pokemon is shiny
+            currentIndex = find_nth(pokemonData, "\n", counter) + 1
+            if pokemonData[currentIndex:currentIndex + 5] == 'Shiny':
+                isShiny = True
+                currentIndex += 13
+                counter = 4
+            else:
+                isShiny = False
+                currentIndex += 1#to be changed soon
+                counter = 3
+            
+            #print("Shiny: ", currentIndex)
+            
+            #getting the pokemon's EV's
+            currentIndex += 5
+            EVs = pokemonData[currentIndex:find_nth(pokemonData, "\n", counter)]
+            
+            #getting the pokemon's nature
+            currentIndex = find_nth(pokemonData, "\n", counter) + 1
+            nature = pokemonData[currentIndex:find_nth(pokemonData, "\n", counter+1)]
+            nature = nature[:-8]
+            counter += 1
+            
+            #print("Nature: ", nature)
+            
+            #getting the pokemon's IV, if any
+            currentIndex = find_nth(pokemonData, "\n", counter) + 1
+            if pokemonData[currentIndex:currentIndex+3] == "IVs":
+                counter += 1
+                IVs = pokemonData[currentIndex+5:find_nth(pokemonData, "\n", counter)]
+                currentIndex = find_nth(pokemonData, "\n", counter)
+            else:
+                IVs = ""
+                currentIndex = find_nth(pokemonData, "\n", counter)
+            
+            #print("IVs: ", IVs)
+        
+            #getting the pokemon's moves
+            currentIndex = find_nth(pokemonData, "\n", counter) + 1
+            newPokemonData = pokemonData[currentIndex:]
+            moves = []
+            for i in newPokemonData.split("\n"):
+                moves.append(i[2:-2])
+            
+            #print("Moves: ", moves)
+        
+            #finally putting all the info into a .json format for visualization
+            newText = {"name":name, "nickname":nickname, "item":item, "ability":ability,
+                       "shiny": isShiny, "evs": EVs, "nature":nature, "ivs": IVs, "moves":moves}
+            
+            #adding a Pokemon class to the team with all these attributes
+            self.team[name] = Pokemon(name, nickname, item, ability, 
+                                      isShiny, EVs, nature, IVs, moves)
     
     def export_showdown_text(self):
         """
@@ -139,3 +217,5 @@ class Team():
     
     def change_nature(self, pokemonName, newNature):
         self.team[pokemonName].change_nature(newNature)
+    
+team = Team("IGL Pokemon Team")
--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import json
+import pokebase as pb
 import discord
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="b!")
 
-with open("pokedex.json") as f:
-    pokedexFile = json.load(f)
-    pokedex = pokedexFile[0]
+
 
 
 
