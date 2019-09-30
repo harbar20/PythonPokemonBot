9843cca07ade5d8919b316e463b581b49f2fd134
harbar20 <30803341+harbar20@users.noreply.github.com>
2019-09-30 12:31:35 -0400

    finished classes.py
    now to validate any information given by the user.

--- classes.py
+++ classes.py
@@ -1,77 +1,135 @@
-import pokebase as pb
+def importpokemon(pokemonstring):
+    """
+    Imports showdown pokemon and returns it as a PokemonClass instance.
+    Code written by TheStraying11 (http://github.com/TheStraying11)
+    """
 
-#getting a list of all pokemon names currently
-with open("pokemon_names.txt") as f:
-    pokemon = [i.replace("\n", "") for i in f]
-    
-#function to find nth instance of substring in a string
-def find_nth(haystack, needle, n):
-    start = haystack.find(needle)
-    while start >= 0 and n > 1:
-        start = haystack.find(needle, start+len(needle))
-        n -= 1
-    return start
-    
-class Pokemon():
-    def __init__(self, pokemonName, nickname="", isShiny=False, item="", 
-                ability="",
-                EVs="", 
-                IVs="", 
-                nature="",
-                moves=["", "", "", ""]):
-        self.name = pokemonName
-        self.nickname = nickname
-        self.isShiny = isShiny
-        self.moves = moves
-        self.item = item
-        self.ability = ability
-        self.ev = EVs
-        self.iv = IVs
-        self.nature = nature
+    keys = ['Ability','Level','Shiny','Happiness','EVs','IVs']
+
+    pokemon = {
+        "Moves": []
+    }
+    first = pokemonstring.strip().split('\n')[0] # gets the first line of the input
+
+    if len(first.split('@')) == 2: # checks for an item and adds it to the pokemon
+        pokemon['Item'] = first.split('@')[1].strip()
+
+    firstNoItem = first.split('@')[0] # gets the first line with the item removed
+
+    if len(firstNoItem.split()) == 1:
+        pokemon['Species'] = firstNoItem
+
+    elif len(firstNoItem.split()) == 2: # deals with a case where there is only one parameter in brackets on the first line
+        if firstNoItem.split()[1] in ['(M)', '(F)']: # checks whether parameter is gender
+            pokemon['Gender'] = firstNoItem.split()[1][1:-1]
+            pokemon['Species'] = firstNoItem.split()[0]
+        else: # deals with the parameter being a species
+            pokemon['Species'] = firstNoItem.split()[1][1:-1]
+            pokemon['Nickname'] = firstNoItem.split()[0]
+    elif len(firstNoItem.split()) == 3: # deals with the case where there is two parameters in the brackets on the first line
+        pokemon['Nickname'] = firstNoItem.split()[0]
+        pokemon['Species'] = firstNoItem.split()[1][1:-1]
+        pokemon['Gender'] = firstNoItem.split()[2][1:-1]
+
+    for i, line in enumerate(pokemonstring.strip().split('\n')): # loop for checking main body of the string
+        if line.split(':')[0].strip() in keys: # this checks through the values listed in the 'keys' list
+            pokemon[line.split(':')[0].strip()] = line.split(':')[1].strip()
+        elif line.strip().startswith('-'): # checking for a line which contains Move data
+            pokemon["Moves"].append(line.strip().replace('- ', ''))
+
+        elif line.endswith('Nature'): # checking for a line which describes the Nature of the pokemon
+            pokemon['Nature'] = line.strip().split()[0].strip()
+
+    for k, v in pokemon.items():
+        if k in ['EVs','IVs']: # performing additional operations on EVs and IVs
+            pokemon[k] = v.split('/') # splits into individual stats
+            pokemon[k] = [l.strip() for l in pokemon[k]] # strips each element
+            pokemon[k] = dict(s.split()[::-1] for s in pokemon[k]) # converts the list into a dictionary
+
+
+    if 'Shiny' in pokemon.keys(): # converts the 'Yes' that Showdown uses to a python Bool
+        pokemon['Shiny'] = True
+    else:
+        pokemon['Shiny'] = False
+
+    returnval = PokemonClass()
+
+    for k, v in pokemon.items(): # sets the attributes of a PokemonClass instance to the values of the corresponding keys
+        setattr(returnval, k, v)
+
+    return returnval
+
+class PokemonClass():
+    def __init__(self, Moves = [], Item = '', Nickname = '', Species = '', Gender = '',
+                Ability = '', Level = '100', Shiny = False, Happiness = '255', 
+                EVs = {}, IVs = {}, Nature = ''):
         
-    def get_name(self):
-        return self.name
-    def get_nickname(self):
-        return self.nickname 
-    def get_isShiny(self):
-        return self.isShiny
-    def get_moves(self):
-        return self.moves
-    def get_item(self):
-        return self.item
-    def get_ability(self):
-        return self.ability
-    def get_ev(self):
-        return self.ev
-    def get_iv(self):
-        return self.iv
-    def get_nature(self):
-        return self.iv
+        self.Moves = Moves
+        self.Item = Item
+        self.Nickname = Nickname
+        self.Species = Species
+        self.Gender = Gender
+        self.Ability = Ability
+        self.Level = Level
+        self.Shiny = Shiny
+        self.Happiness = Happiness
+        self.EVs = EVs
+        self.IVs = IVs
+        self.Nature = Nature
     
+    #accessor functions
+    def get_Species(self):
+        return self.Species
+    def get_Nickname(self):
+        return self.Nickname 
+    def get_Gender(self):
+        return self.Gender
+    def get_Shiny(self):
+        return self.Shiny
+    def get_Happiness(self):
+        return self.Happiness
+    def get_Moves(self):
+        return self.Moves
+    def get_Item(self):
+        return self.Item
+    def get_Ability(self):
+        return self.Ability
+    def get_EVs(self):
+        return self.EVs
+    def get_IVs(self):
+        return self.IVs
+    def get_Nature(self):
+        return self.Nature
+    
+    #mutator functions
     def change_nickname(self, newNickname):
-        self.nickname = newNickname      
-    def toggle_isShiny(self):
-        if self.isShiny:
-            self.isShiny = False
+        self.Nickname = newNickname 
+    def toggle_Gender(self, newGender):
+        self.Gender = newGender    
+    def toggle_Shiny(self):
+        if self.Shiny:
+            self.Shiny = False
         else:
-            self.isShiny = True
-    def change_move(self, oldMove, newMove):
-        self.moves[self.moves.index(oldMove)] = newMove
-    def change_item(self, newItem):
-        self.item = newItem
-    def change_ability(self, newAbility):
-        self.ability = newAbility
-    def change_ev(self, evStat, evNum):
-        self.ev[evStat] = evNum
-    def change_iv(self, ivStat, ivNum):
-        self.iv[ivStat] = ivNum
-    def change_nature(self, newNature):
-        self.nature = newNature
-    
-class Team():
-    def __init__(self, team_name):
+            self.Shiny = True
+    def change_Happiness(self, newHappiness):
+        self.Happiness = newHappiness
+    def change_Move(self, oldMove, newMove):
+        self.Moves[self.Moves.index(oldMove)] = newMove
+    def change_Item(self, newItem):
+        self.Item = newItem
+    def change_Ability(self, newAbility):
+        self.Ability = newAbility
+    def change_EVs(self, evStat, evNum):
+        self.EVs[evStat] = evNum
+    def change_IVs(self, ivStat, ivNum):
+        self.IVs[ivStat] = ivNum
+    def change_Nature(self, newNature):
+        self.Nature = newNature
+
+class TeamClass():
+    def __init__(self, team_name=''):
         self.team_name = team_name
-        self.team = {}
+        self.team = []
     
     def get_team(self):
         return self.team
@@ -79,179 +137,75 @@ class Team():
     def get_team_name(self):
         return self.team_name
     
-    #TODO: port showdown's import code
     def import_showdown_text(self, showdown_text):
         """
-        Imports showdown teams and saves it as the team.
+        Imports pokemon from showdown teams and appends them to self.team.
+        Code written by TheStraying11 (http://github.com/TheStraying11)
         """
-        #separating the text by pokemon
-        pokemonTextList = showdown_text.split("\n\n")
-        
-        #going through each pokemon data in the list
-        numIterations = 1
-        for pokemonData in pokemonTextList:
-            if numIterations > 6:
-                break
-            counter = 2
-            #getting name/nickname from pokemon
-            currentIndex = pokemonData.find("@") - 1
-            name = pokemonData[:currentIndex]
-            #looking through list of all pokemon names to see if they have nicknames
-            if name not in pokemon:
-                nickname = name[:pokemonData.find("(") - 1]
-                name = name[pokemonData.find("(")+1:pokemonData.find(")")]
-            else:
-                nickname = ""
-            #print("Nickname: ", currentIndex)
-            
-            #getting the item of the pokemon
-            currentIndex += 3
-            item = pokemonData[currentIndex:pokemonData.find("\n")]
-            #print("Item: ", item)
-            
-            #getting the ability of the pokemon
-            currentIndex = pokemonData.find("\n") + 10
-            ability = pokemonData[currentIndex:find_nth(pokemonData, "\n", counter)]
-            #print("Ability: ", currentIndex)
-            
-            #checking if the pokemon is shiny
-            currentIndex = find_nth(pokemonData, "\n", counter) + 1
-            if pokemonData[currentIndex:currentIndex + 5] == 'Shiny':
-                isShiny = True
-                currentIndex += 13
-                counter = 4
-            else:
-                isShiny = False
-                currentIndex += 1
-                counter = 3
-            
-            #print("Shiny: ", currentIndex)
-            
-            #getting the pokemon's EV's
-            currentIndex += 2
-            EVs = pokemonData[currentIndex:find_nth(pokemonData, "\n", counter)]
-            
-            #getting the pokemon's nature
-            currentIndex = find_nth(pokemonData, "\n", counter) + 1
-            nature = pokemonData[currentIndex:find_nth(pokemonData, "\n", counter+1)]
-            if "Nature" in nature:
-                nature = nature[:-7]
-                counter += 1
-            else:
-                nature = "Serious"
-            
-            #print("Nature: ", nature)
-            
-            #getting the pokemon's IV, if any
-            currentIndex = find_nth(pokemonData, "\n", counter) + 1
-            if pokemonData[currentIndex:currentIndex+3] == "IVs":
-                counter += 1
-                IVs = pokemonData[currentIndex+5:find_nth(pokemonData, "\n", counter)]
-                currentIndex = find_nth(pokemonData, "\n", counter)
-            else:
-                IVs = ""
-                currentIndex = find_nth(pokemonData, "\n", counter)
-            
-            #print("IVs: ", IVs)
-        
-            #getting the pokemon's moves
-            currentIndex = find_nth(pokemonData, "\n", counter) + 1
-            newPokemonData = pokemonData[currentIndex:]
-            moves = []
-            for i in newPokemonData.split("\n"):
-                moves.append(i[2:])
-            
-            #print("Moves: ", moves)
-        
-            #finally putting all the info into a .json format for visualization
-            newText = {"name":name, "nickname":nickname, "item":item, "ability":ability,
-                       "shiny": isShiny, "evs": EVs, "nature":nature, "ivs": IVs, "moves":moves}
-            
-            #print(newText)
-            
-            #adding a Pokemon class to the team with all these attributes
-            self.team[name] = Pokemon(name, nickname, isShiny, item, ability, 
-                                      EVs, IVs, nature, moves)
-
-            numIterations += 1
+        showdown_text = showdown_text.strip().split('\n\n') # split into individual pokemon
+        for i in showdown_text:
+            pokemon = importpokemon(i) # create PokemonClass instance from the import string
+            self.team.append(pokemon) # append the instance to the team list
     
     #TODO: port showdown's export code
     def export_showdown_text(self):
         """
         Effectively exports team in Showdown team format.
         """
-        #literally just returns it in showdown format with string formatting
-        returnStr = ""
+        teamString = """"""
         
-        #random counter to make sure count doesn't go more than 6.
-        i = 1
-        for pokemon, pokemonData in self.team.items():
-            if i > 6:
-                break
-            #decides if pokemon is shiny
-            if pokemonData.isShiny:
-                shiny = "Yes"
-            else:
-                shiny = "No"
+        #formatting it
+        for pokemon in self.team:
+            teamString += f"""
+            {pokemon.get_Nickname()} ({pokemon.get_Species()}) ({pokemon.get_Gender()}) @ {pokemon.get_Item()}
+            Ability: {pokemon.get_Ability()}
+            Level: {pokemon.get_Level()}
+            Shiny: {pokemon.get_Shiny}
+            Happiness: {pokemon.get_Happiness()}
+            EVs: {pokemon.get_EVs()}
+            {pokemon.get_Nature()} Nature
+            IVs: {pokemon.get_IVs}
+            - {pokemon.get_Moves[0]}
+            - {pokemon.get_Moves[1]}
+            - {pokemon.get_Moves[2]}
+            - {pokemon.get_Moves[3]}
             
-            #making the text. this 'f' before the string is NOT A TYPO.
-            returnStr += f""" 
-{pokemonData.nickname} ({pokemonData.name}) @ {pokemonData.item} 
-Ability: {pokemonData.ability} 
-Shiny: {shiny} 
-EVs: {pokemonData.ev} 
-{pokemonData.nature} Nature 
-IVs: {pokemonData.iv} 
-- {pokemonData.moves[0]} 
-- {pokemonData.moves[1]} 
-- {pokemonData.moves[2]} 
-- {pokemonData.moves[3]} 
-"""
-            i += 1
+            """
         
-        print(returnStr)
-        return returnStr
+        return teamString
     
-    def get_pokemon_info(self, pokemonName):
-        return self.team[pokemonName]
+    def get_pokemon_info(self, pokemonID):
+        return self.team[pokemonID+1]
     
     def change_team_name(self, newName):
         self.team_name = newName
     
-    def add_pokemon(self, pokemonName, nickname="", level="100", gender="F", isShiny=False,
-                    moves=[], item="", ability="",
-                    EVs={}, IVs={}, nature=""):        
-        self.team[pokemonName] = Pokemon(pokemonName, nickname, level, gender, isShiny,
-                                         moves, item, ability,
-                                         EVs, IVs, nature)
+    def add_pokemon(self, Moves = [], Item = '', Nickname = '', Species = '', Gender = 'F',
+                Ability = '', Level = '100', Shiny = False, Happiness = '255', EVs = {}, IVs = {}, Nature = ''):        
+        self.team[pokemonID+1] = PokemonClass(Moves, Item, Nickname, Species, Gender, Ability,
+                                        Level, Shiny, Happiness, EVs, IVs, Nature)
     
-    def remove_pokemon(self, pokemonName):
-        self.team.remove(pokemonName)
+    def remove_pokemon(self, pokemonID):
+        self.team.pop(pokemonID+1)
     
-    def change_nickname(self, pokemonName, newNickname):
-        self.team[pokemonName].change_nickname(newNickname)
+    def change_nickname(self, pokemonID, newNickname):
+        self.team[pokemonID+1].change_nickname(newNickname)
 
-    def change_move(self, pokemonName, oldMove, newMove):
+    def change_move(self, pokemonID, oldMove, newMove):
         selfTeam = self.get_team()
-        self.team[pokemonName].moves[selfTeam.index(oldMove)] = newMove
+        self.team[pokemonID+1].moves[selfTeam.index(oldMove)] = newMove
     
-    def change_item(self, pokemonName, item):
-        self.team[pokemonName].change_item(item)
+    def change_item(self, pokemonID, item):
+        self.team[pokemonID+1].change_item(item)
     
-    def change_ability(self, pokemonName, newAbility):
-        self.team[pokemonName].change_ability(newAbility)
+    def change_ability(self, pokemonID, newAbility):
+        self.team[pokemonID+1].change_ability(newAbility)
     
-    def change_ev(self, pokemonName, evStat, evNum):
-        self.team[pokemonName].change_ev(evStat, evNum)
+    def change_ev(self, pokemonID, evStat, evNum):
+        self.team[pokemonID+1].change_ev(evStat, evNum)
     
-    def change_iv(self, pokemonName, ivStat, ivNum):
-        self.team[pokemonName].change_iv(ivStat, ivNum)
+    def change_iv(self, pokemonID, ivStat, ivNum):
+        self.team[pokemonID+1].change_iv(ivStat, ivNum)
     
-    def change_nature(self, pokemonName, newNature):
-        self.team[pokemonName].change_nature(newNature)
-    
-team = Team("IGL Pokemon Team")
-
-team.import_showdown_text("""
-""")
-team.export_showdown_text()
+    def change_nature(self, pokemonID, newNature):
+        self.team[pokemonID+1].change_nature(newNature)
